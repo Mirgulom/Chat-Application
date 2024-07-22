d3eaa8029675bb443ec5d67aed3f497ce717c73d
Mirsolihov <mychatgptmirsolihov@gmail.com>
2024-07-22 11:37:53 +0500

    Connected to data

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 
@@ -14,3 +24,4 @@ app.get("/", (req,res) => {
 app.listen(PORT, () => {
     console.log("Server is running on port" + " " + PORT)
 })
+
