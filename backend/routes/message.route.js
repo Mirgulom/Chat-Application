c1c289d49789945c517dd434c221ddc1ac598b8b
Mirsolihov <mychatgptmirsolihov@gmail.com>
2024-07-23 15:02:01 +0500

    get message api

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { isAuthenticate } from "../middleware/verifyToken.js"
-import { sendMessage } from "../controller/message.controller.js"
+import { sendMessage, getMessage } from "../controller/message.controller.js"
 
 const router = express.Router()
 
+router.get("/:id", isAuthenticate, getMessage) 
 router.post("/send/:id", isAuthenticate, sendMessage) 
 
 export default router
