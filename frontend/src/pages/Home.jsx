7aad98f13ef3aada83eae66674ec2cb3067a76f2
Mirsolihov <mychatgptmirsolihov@gmail.com>
2024-07-24 17:36:16 +0500

    Message Container

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import MessageContainer from "../components/messages/MessageContainer";
 import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
     return ( 
         <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
             <Sidebar/>
-            
+
+            <MessageContainer />
         </div>
      );
 }
